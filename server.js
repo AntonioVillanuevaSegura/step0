820d0946998bc46978c25e6c8a706e7b98ab27ab
icaro <icaro@localhost.localdomain>
2025-12-09 14:40:00 +0100

    anado static

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 const app=express()
+app.use (express.static ('public'))
 app.use (bodyParser.json())
 app.get ('/api/v1/whisper', (req,res) => {
 	res.json([])
@@ -19,4 +20,6 @@ app.put ('api/v1/whisper/:id',(req,res) => {
 	//const id=parseInt (req.params.id)
 	res.sendStatus(200)
 })
+
+
 export {app}
