2d643d5c05e2ed18fe0db56d5ebd3474788ed65e
icaro <icaro@localhost.localdomain>
2025-12-12 13:22:31 +0100

    comentarios

--- tests/store.test.js
+++ tests/store.test.js
@@ -4,24 +4,20 @@ import {whispers,inventedId,existingId} from './fixtures.js'
 import {writeFileSync} from 'node:fs'
 import { join } from 'node:path'
 import {describe, it, expect, beforeEach, afterAll} from '@jest/globals' 
-
-//const dbPath = join (process.cwd(),'db.json')
-
-//const restoreDb = () =>writeFileSync (dbPath,JSON.stringify ([]))
-//const populateDb = (data) =>writeFileSync (dbPath,JSON.stringify (data))
-
-//Fixtures conjunto de datos predefinidos y fijos, para probar la Db
-//const whispers = [ {id:1,message:'test'} ,{id:2,message:'hello world'}]
-
-//const inventedId = 12345
-
-//const existingId = whispers [0].id
+/*
+ Jest, la librería de pruebas para JavaScript/Node.js
+describe(): Agrupa tests relacionados en "suites" (bloques lógicos).
+it() (o test()): Define un test individual (una prueba específica).
+expect(): Crea aserciones (verifica que algo sea verdadero).
+beforeEach(): Ejecuta código de setup antes de cada test (reinicia estado).
+afterAll(): Ejecuta código de cleanup una vez después de todos los tests del archivo o suite.
+ */
 
 //describe testing con frameworks
 //Test para store
-describe ('store', () => { // Nivel 1: Módulo completo
-	beforeEach ( () =>populateDb (whispers))
-	afterAll (restoreDb)
+describe ('store', () => { // Nivel 1: test Módulo completo store.js
+	beforeEach ( () =>populateDb (whispers)) //Antes de cada test llenar whispers
+	afterAll (restoreDb) //despues restaurar la db
 	//Aqui van anidados todos los tests 
 			
 	//Test para getAll
@@ -29,14 +25,14 @@ describe ('store', () => { // Nivel 1: Módulo completo
 		
 		it ("Retorna un array vacio si no hay datos",async () =>{
 			restoreDb()
-			const data = await getAll()
+			const data = await getAll() //Recupera fichero local db.json
 			//expect (data).toEqual (fixtures)
-			 expect(data).toEqual([])  // Cambia fixtures por []
+			 expect(data).toEqual([]) //data esta vacio es igual a []
 		})
 		
 		it ("Retorna un array con un item cuando hay un item",async () => {
-			const data = await getAll()
-			expect(data).toEqual(whispers)
+			const data = await getAll() //Recupera fichero local db.json
+			expect(data).toEqual(whispers) //whispers en un JSON de test en fixtures.js
 		})	
 		
 	})
@@ -44,13 +40,13 @@ describe ('store', () => { // Nivel 1: Módulo completo
 	//Tests para getById
 	describe ('getById', () =>{
 		it ("Retorna undefined cuando no hay elemento con la id",async () =>{
-			const item = await getById (inventedId)
-			expect (item).toBeUndefined()
+			const item = await getById (inventedId) //busca la id inventedId = 12345
+			expect (item).toBeUndefined() //indefinido 
 		})	
 		
 		it ("retorna el item con la id",async () =>{
-			const item = await getById (whispers[0].id)
-			expect (item).toEqual (whispers[0])
+			const item = await getById (whispers[0].id)//busca la id:1 , {id:1 ,message :'test'},
+			expect (item).toEqual (whispers[0]) //espera {id:1 ,message :'test'}
 		})	
 		
 	})
@@ -58,7 +54,7 @@ describe ('store', () => { // Nivel 1: Módulo completo
 	//Esperamos que el elemento sea devuelto incluyendo el id y anadido a la Db
 	describe ('create', () =>{
 		it ("Debe retornar el item creado ",async () =>{
-			const newItem = {id:whispers.length+1,message: 'test 3'}
+			const newItem = {id:whispers.length+1,message: 'test 3'} //Crea un item con mensaje test 3
 			const item = await create (newItem.message)
 			expect (item).toEqual (newItem)
 			
